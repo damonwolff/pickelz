5e5ea5569f2bd53dca3bdd55912ba796b72e71ec
damonwolff <31035434+damonwolff@users.noreply.github.com>
2017-10-09 00:28:27 +0200

    Spawns some aliens, but can't get collision detection right

--- pickelz-master/SpaceInvaders/SpaceInvaders/Bullet.cs
+++ pickelz-master/SpaceInvaders/SpaceInvaders/Bullet.cs
@@ -8,42 +8,51 @@ using System.Windows.Media.Imaging;
 using System.Windows;
 using System.Windows.Controls;
 
-namespace SpaceInvaders
-{
-    class Bullet
-    {
-        public Image bullet; 
+namespace SpaceInvaders {
+    class Bullet {
+        public Image bullet;
         public double PosX { get; set; }
         public double PosY { get; set; }
+        public double width { get; set; }
+        public string identity { get; set; }
         public Canvas c;
         public List<Image> bullets = new List<Image>();
-        
 
-        public void forward ()
-        {
 
-            foreach(Image b in bullets) {
+        public void forward() {
+
+            foreach (Image b in bullets) {
                 //PosY += 5;
                 double y = Canvas.GetBottom(b);
-            Canvas.SetBottom(b, y+5);
+                Canvas.SetBottom(b, y + 5);
                 if (Canvas.GetTop(b) < 0) {
                     c.Children.Remove(b);
                 }
             }
         }
-        public Bullet( Canvas space, double x)
-        {
+        public Bullet(Canvas space, double x, string i) {
             bullet = new Image();
+            bullet.Name = "n"+i;
+            identity ="n"+ i;
             space.Children.Add(bullet);
             c = space;
             bullet.Source = new BitmapImage(new Uri($"pack://application:,,,/Bullet.png")); // dont know how images work really 
             bullet.Width = 10;
             bullet.Height = 15;
 
-            Canvas.SetLeft(bullet,x);
+            Canvas.SetLeft(bullet, x);
             Canvas.SetBottom(bullet, 50);
-            PosY = Canvas.GetBottom(bullet);
+            PosY = Canvas.GetTop(bullet);
+            PosX = Canvas.GetLeft(bullet);
+            width = bullet.ActualWidth;
             bullets.Add(this.bullet);
+        }
+
+        public void delete(string b) {
+
+
+            var thing = (UIElement)LogicalTreeHelper.FindLogicalNode(c, b);
+            c.Children.Remove(thing);
         }
     }
 }
--- pickelz-master/SpaceInvaders/SpaceInvaders/MainWindow.xaml.cs
+++ pickelz-master/SpaceInvaders/SpaceInvaders/MainWindow.xaml.cs
@@ -26,8 +26,11 @@ namespace SpaceInvaders {
         state shipState;
         Ship player;
         Bullet b;
+        int noBull = 0;
         List<Bullet> bullets = new List<Bullet>();// thought having a list of bullets in the bullet class would be enough but this seems to work I'm just gonna leave it like this
         int firingInterval = 0; //so there's not a constant fire rate
+        public int level = 1;
+        List<Alien> aliens = new List<Alien>();
 
         public MainWindow() {
             InitializeComponent();
@@ -37,12 +40,40 @@ namespace SpaceInvaders {
             theTimer.IsEnabled = true;
             theTimer.Tick += dispatcherTimer_Tick;
             player = new Ship(space);
+            addAliens(level);
+            
+        }
+
+        void addAliens(int lev) {
+            double x =50;
+            double y =15;
+            for (int i=0; i < lev * 15; i++) {
+                Alien a = new Alien(space,x,y,Convert.ToString(i));
+                aliens.Add(a);
+                x = x + 30;
+            }
+        }
+
+        public void checkCollisions() {
+            foreach(Bullet b in bullets) {
+                foreach(Alien a in aliens) {
+                    if (b.PosX >= a.PosX) {// && b.PosY<=a.PosY+a.height) {
+                        
+                        b.delete(b.identity);
+                        a.delete(a.identity);
+                        //bullets.Remove(b);
+                        //aliens.Remove(a);
+                        
+                    }
+                }
+            }
         }
 
         public void dispatcherTimer_Tick(object sender, EventArgs e) {
             firingInterval++;
             updateShip();
             updateBullets();
+            checkCollisions();
         }
 
         void updateBullets() {
@@ -62,7 +93,8 @@ namespace SpaceInvaders {
                     break;
                 case state.shooting:
                     if (firingInterval > 25) {
-                        b = new Bullet(space, player.actualX);
+                        b = new Bullet(space, player.actualX, Convert.ToString(noBull));
+                        noBull++;
                         bullets.Add(b);
                         firingInterval = 0;
                     }
